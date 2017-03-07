ed827efa5d96cbd8a549004eac5ac437c847df69
Noah <noah.oberman@gmail.com>
2017-03-07 05:03:11 -0500

    finished all of the first part and got dicey to work, my bug was to have the name and the query property names the same

--- src/dicey/dicey.js
+++ src/dicey/dicey.js
@@ -2,31 +2,123 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/about', (req,res) => {
-  res.render('about');
+
+//function for generating number
+function diceRoll(){
+  return Math.floor((Math.random()*6)+1);
+}
+//function for generating 5 numbers
+function diceRolls(){
+  let temp = '';
+  for(let i =0; i<5; i++){
+    temp += diceRoll()+'';
+  }
+  return temp;
+}
+//reads the file
+fs.readFile('./diceware.wordlist.txt','utf8', (err,data) => {
+  let numWords = 0;
+  let glue = '';
+  let words = {};
+  let arr = data.split(/\n|\t/);
+  let arrOfNums = [];
+  let arrOfWords = [];
+  let flag = false;
+  //puts all the words in as object
+  for(let i=0; i<data.length-1; i+=2){
+    words[arr[i]] = arr[i+1];
+  }
+
+
+
+  app.get('/about', (req,res) => {
+    res.render('about');
+  });
+  app.get('/base.css', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/css','base.css'));
+  });
+  app.use('/dice', (req,res,next) => {
+    glue = req.query.glue;
+    numWords = req.query.numWords;
+    console.log(req.body)
+    console.log(req.query.glue);
+    console.log(req.query.numWords);
+    next();
+  });
+  app.get('/dice', (req,res) => {
+    if(req.query.numWords !== undefined){
+      numWords = req.query.numWords;
+      //put the words with corresponding numbers in 2 arrays
+      for(let i=0; i<numWords;i++){
+        let property = diceRolls();
+        arrOfNums.push(property);
+        arrOfWords.push(words[property]);
+      }
+      //console.log(arrOfWords);
+      //console.log(arrOfNums);
+      //flag to signal that there are words
+      flag = true;
+    }
+    if(req.query.glue !== undefined){
+      glue = req.query.glue;
+    }else{
+      glue = 'space';
+    }
+    let answer = '';
+    for(let i =0; i<numWords; i++){
+      answer += arrOfWords[i];
+      if(i != numWords-1){
+        if(glue === 'space'){
+          answer += " ";
+        }else if(glue === 'dash'){
+          answer += '-';
+        }else if(glue === 'star'){
+          answer += '*';
+        }else if (glue === 'none'){
+          answer+= '';
+        }else if(glue === 'comma'){
+          answer += ',';
+        }
+      }
+    }
+    answerWords = '';
+    answerNums = '';
+    for(let i = 0; i<numWords; i++){
+      answerWords += arrOfWords[i] + '    ';
+      answerNums += arrOfNums[i] + '    ';
+    }
+    //console.log(arrOfNums);
+    // res.render('index', words, glue, arrOfNums);
+    res.render('index', {'numWords':numWords, 'answerWords': answerWords, 'answerNums':answerNums, 'words': words, 'glue': glue, 'arrOfNums': arrOfNums, 'flag': flag, 'arrOfWords': arrOfWords, 'answer': answer});
+
+
+    // arrOfNums = [];
+    // arrOfWords = [];
+    flag = false;
+    //res.redirect('dice');
+    // console.log(numWords);
+    // console.log(glue);
+
+  });
+
+
+  // app.post('/dice', (req, res) => {
+  //   console.log('handling post', req.body.number);
+  //   numWords = (req.body.numWords);
+  //   res.redirect('dice');
+  // });
+
+
+  app.get('/', (req,res) => {
+    res.redirect('/dice');
+  });
+
+  app.listen(8080);
+
 });
-app.get('/base.css', (req,res) => {
-  res.sendFile(path.join(__dirname, '/public/css','base.css'));
-});
-
-app.get('/dice', (req,res) => {
-  res.render('index');
-  //console.log(req.query);
-  res.render('simple-form', {'myName':myName});
-});
-
-app.get('/', (req,res) => {
-  res.redirect('/dice');
-});
-
-app.post('/', function(req, res) {
-	console.log(req.body);
-	// change the global
-	myName = req.body.myName;
-	res.redirect('/');
-});
-
-
-app.listen(8080);
--- src/foodz/foodz.js
+++ src/foodz/foodz.js
@@ -1,1 +1,26 @@
 // foodz.js
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req,res) => {
+  res.redirect('/foodz');
+});
+
+app.get('/foodz', (req,res) => {
+  res.render('index');
+});
+
+app.get('/base.css', (req,res) => {
+  res.sendFile(path.join(__dirname, '/public/css','base.css'));
+  //console.log(path.join(__dirname, '/public/css','base.css'));
+});
+
+
+
+app.listen(8080);
