004b48039e98a739cf5c1efae31b53bbb62e64c6
Noah <noah.oberman@gmail.com>
2017-03-10 10:07:45 -0500

    got all of the CSS done just need to add the finishing touches on the GET and POST methods

--- src/foodz/foodz.js
+++ src/foodz/foodz.js
@@ -5,6 +5,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+
+
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -19,6 +21,12 @@ app.get('/foodz', (req,res) => {
 app.get('/base.css', (req,res) => {
   res.sendFile(path.join(__dirname, '/public/css','base.css'));
   //console.log(path.join(__dirname, '/public/css','base.css'));
+});
+
+app.post('/foodz', (req,res) => {
+  console.log(req.body);
+
+  res.redirect('/foodz');
 });
 
 
